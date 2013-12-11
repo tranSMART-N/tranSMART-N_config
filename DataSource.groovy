55ae17683411c8646968d4b2bd1def19d34d0aa4
Haiping Xia <haxia@deloitte.com>
2013-12-11 16:27:05 -0500

    add 2nd datasource for transaction

--- DataSource.groovy
+++ DataSource.groovy
@@ -3,22 +3,34 @@
  * by the tranSMART application when the tomcat is restarted
  */
 
-
+// main datasource
 dataSource {
     driverClassName ="org.netezza.Driver"
     url = "jdbc:netezza://192.168.139.128:5480/transmart"	
     dialect = "org.cdl.NetezzaDialect"
     username = "biomart_user"
     password = "biomart_user"
-	
+	}
+
+ // 2nd datasource for transaction using Netezza
+ dataSource_tx {
+     driverClassName ="org.netezza.Driver"
+     url = "jdbc:netezza://192.168.139.128:5480/transmart"
+     dialect = "org.cdl.NetezzaDialect"
+     username = "biomart_user"
+     password = "biomart_user"
+ }
+
 /*
+ // 2nd datasource for transaction using PostgreSQL
+ dataSource_tx {
     driverClassName ="org.postgresql.Driver"
     url = "jdbc:postgresql://localhost:5432/transmart"
     username = "biomart_user"
     password = "biomart_user"
     dialect = "org.hibernate.dialect.PostgreSQLDialect"
+ }
 */
-	}
 
 /*
 dataSource {
